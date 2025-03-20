fe7401aed8f7e5e767d88c4e2dedddd50f52e6ac
johnilluminati <johnbilluminati@gmail.com>
2025-03-20 04:19:58 -0400

    Update DarkModeToggle.jsx

--- political-education/src/components/Header/DarkModeToggle.jsx
+++ political-education/src/components/Header/DarkModeToggle.jsx
@@ -1,14 +1,43 @@
+import { useEffect, useState } from "react";
+
 const DarkModeToggle = () => {
+  const storedTheme = localStorage.getItem('theme');
+  const [isDarkMode, setIsDarkMode] = useState((storedTheme && storedTheme === 'dark') || false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (isLoading && (storedTheme && storedTheme === 'dark')) {
+      setIsDarkMode(true);
+    }
+    setIsLoading(false);
+  }, [isDarkMode, isLoading, storedTheme]);
+
+  useEffect(() => {
+    const toggleVal = isDarkMode ? 'dark' : 'light';
+
+    if (!storedTheme || (storedTheme && storedTheme !== toggleVal)) {
+      localStorage.setItem('theme', toggleVal);
+    }
+
+    document.documentElement.classList.toggle('dark', isDarkMode);
+  }, [isDarkMode, storedTheme]);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  }
+
   return (
     <label
-      htmlFor="AcceptConditions"
+      htmlFor="DarkModeToggle"
+      id="lblDarkModeToggle"
       className="relative inline-block h-8 w-14 cursor-pointer rounded-full bg-gray-300 transition [-webkit-tap-highlight-color:_transparent] has-[:checked]:bg-gray-700"
     >
       <input
         type="checkbox"
-        id="AcceptConditions"
+        id="DarkModeToggle"
         className="peer sr-only"
-        onClick={() => document.documentElement.classList.toggle('dark')}
+        checked={isDarkMode}
+        onChange={toggleTheme}
       />
 
       <span
